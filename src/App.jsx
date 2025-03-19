aa5ba3f7c2e4cd866e2c7381d5edb8457498e16e
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-03-19 08:15:10 +0530

    Adjusted margins

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ParentTabs from "./components/tabs/parentTabs";
 
 function App() {
   return (
-    <main className="flex justify-center items-center mx-40 p-10">
+    <main className="flex justify-center items-center sm:m-0 md:mx-40 lg:mx-60 p-10 xl:80">
       <ParentTabs />
     </main>
   );
